fadd3db8959b32dfa3c37b72b3d8b9f8ddae767b
Ikhsanuddin - Yamisok <mail.ikhsanuddin@gmail.com>
2017-10-23 17:21:15 +0700

    instagram feed

---
+++ src/components/InstaFeed.vue
@@ -0,0 +1,23 @@
+<template>
+  <vue-instagram token="accessTokenHere" username="kevinongko" :count="5">
+    <template slot="feeds" scope="props">
+      <li class="fancy-list"> {{ props.feed.link }} </li>
+    </template>
+    <template slot="error" scope="props">
+      <div class="fancy-alert"> {{ props.error.error_message }} </div>
+    </template>
+  </vue-instagram>
+
+</template>
+ 
+<script>
+import VueInstagram from 'vue-instagram'
+ 
+export default {
+  name: 'App',
+ 
+  components: {
+    VueInstagram
+  }
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import VueInstagram from 'vue-instagram'
 
+Vue.use(VueInstagram)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
