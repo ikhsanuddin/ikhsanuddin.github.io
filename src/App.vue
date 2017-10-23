f1bbf23291b897cefc73e2e2ff4cdd127b5c26e1
Ikhsanuddin - Yamisok <mail.ikhsanuddin@gmail.com>
2017-10-23 17:01:47 +0700

    tampilan awal

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,44 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
-  </div>
+  <main id="app">
+    <h1>
+      Test
+    </h1>
+    <section class="flex flex-align-center flex-justify-center">
+      <h2>Overview</h2>
+      <div class="overflow-0 p-4">
+        <div class="col-6">
+          <div class="card round">
+            <div class="card-header">
+              <img src="" class="avatar" alt="">
+            </div>
+            <div class="card-body">
+              <div class="item-sq" style="background-image: url(http://abduzeedo.com/sites/default/files/styles/home_cover/public/originals/coverui.jpg?itok=hy01bELG)">
+              </div>
+            </div>
+            <div class="card-desc">
+              Haha hihi<br>yohasdasd asjdaksdh kalsdkj<br>kasljd
+            </div>
+          </div>
+        </div>
+
+        <div class="col-6">
+          <div class="card round">
+            <div class="card-header">
+              <img src="" class="avatar" alt="">
+            </div>
+            <div class="card-body">
+              <div class="item-sq" style="background-image: url(http://abduzeedo.com/sites/default/files/styles/home_cover/public/originals/coverui.jpg?itok=hy01bELG)">
+              </div>
+            </div>
+            <div class="card-desc">
+              Haha hihi<br>yohasdasd asjdaksdh kalsdkj<br>kasljd
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </section>
+  </main>
 </template>
 
 <script>
@@ -17,12 +53,109 @@ export default {
 </script>
 
 <style>
+body{
+  margin: 0;
+}
+
+*{
+  webkit-box-sizing: border-box;
+  -moz-box-sizing: border-box;
+  box-sizing: border-box;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+h1{
+  position: fixed;
+  top: 20px;
+  left: 20px;
+}
+
+.round{
+  border-radius: 10px;
+}
+
+.flex{
+  display: flex;
+}
+
+.flex-align-center{
+  align-items: center;
+}
+
+.flex-justify-center{
+  justify-content: center;
+}
+
+section{
+  min-height: 100vh;
+}
+
+.item-sq{
+  width: 100%;
+  padding-top: 100%;
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-color: beige;
+}
+
+.overflow-0{
+  overflow: hidden;
+}
+
+.card{
+  background-color: beige;
+  border: 1px solid #fafafa;
+  box-shadow: 0px 0px 50px 0px rgba(0,0,0,0.05);
+  transition: all 0.2s ease;
+}
+
+.card:hover{
+  filter: brightness(1.02);
+}
+.card:active{
+  transform: scale(0.97);
+  box-shadow: 0px 0px 35px 0px rgba(0,0,0,0.05);
+}
+
+.card .card-desc, 
+.card .card-header{
+  padding: 15px;
+}
+
+.col-12{
+  width: 100%;
+}
+
+.col-6{
+  width: 50%;
+}
+
+.col-4{
+  width: 33.333%;
+}
+
+.col-3{
+  width: 25%;
+}
+
+.col-2{
+  width: 16.667%;
+}
+
+.col-1,.col-2,.col-3,.col-4,.col-5,.col-6,.col-7,.col-8,.col-9,.col-10,.col-11,.col-12{
+  padding-left: 15px;
+  padding-right: 15px;
+  float: left;
+  clear: none;
+}
+
+.p-4{
+  padding: 40px;
 }
 </style>
